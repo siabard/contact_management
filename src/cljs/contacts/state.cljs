ce2fb6284f92259b42237769ae838c8d4a015e90
Yeonho Jang <siabard@cardong.co.kr>
2026-07-10 17:36:39 +0900

    수정, 삭제, 추가 관련 reducer 의 추가

--- src/cljs/contacts/state.cljs
+++ src/cljs/contacts/state.cljs
@@ -20,12 +20,38 @@
   ::set-selected [state [_ payload]]
   (assoc state :selected payload))
 
+(defmethod app-reducer
+  ::add-contact [state [_ payload]]
+  (let [prev (:contacts state)
+        next (conj prev payload)]
+    (assoc state :contacts next)))
+
+(defmethod app-reducer
+  ::update-contact [state [_ payload]]
+  (let [prev (:contacts state)
+        update-contact #(if (= (:id %) (:id payload))
+                          payload
+                          %)
+        next (map update-contact prev)]
+    (assoc state :contacts next)))
+
+(defmethod app-reducer
+  ::remove-contact [state [_ payload]]
+  (let [prev (:contacts state)
+        not-matching (fn [item]
+                       (not (= (:id item) (:id payload))))
+        next (filter not-matching prev)]
+    (assoc state :contacts next)))
+
 (comment
-  (app-reducer initial-state [:set-contacts []])
+  (app-reducer initial-state [::set-contacts []])
 )
 
 (defn use-app-state []
   (let [[state dispatch] (hooks/use-context app-state)]
     [state {:init (fn [response]
                     (dispatch [::set-contacts response])
-                    (dispatch [::set-selected (first response)]))}]))
+                    (dispatch [::set-selected (first response)]))
+            :add-contact #(dispatch [::add-contact %])
+            :update-contact #(dispatch [::update-contact %])
+            :delete-contact #(dispatch [::remove-contact %])}])) 
